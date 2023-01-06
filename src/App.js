dbfb727db8af0d8f7a423032d051d98cba601b7c
Morteza Javanbakht <114153047+MortezaJavanbakht@users.noreply.github.com>
2023-01-06 16:37:17 +0330

    edit total Price

--- src/App.js
+++ src/App.js
@@ -1,15 +1,75 @@
 import "./App.css";
 import Card from "./components/Card/Card";
-import bergerImage from "./assets/Image/salad.png"
+import burger from "./assets/Image/hamburger.png";
+import sib from "./assets/Image/french_fries.png";
+import salad from "./assets/Image/salad.png";
+import soda from "./assets/Image/soda.png";
+import { useState } from "react";
+import tick from "./assets/Image/tick.png";
+
+const foods = [
+  {
+    title: "همبرگر معمولی",
+    price: 8000,
+    id: "burger",
+    imgSrc: burger,
+  },
+  {
+    title: "همبرگر مخصوص",
+    price: 10000,
+    id: "specialBurger",
+    imgSrc: burger,
+  },
+  {
+    title: "همبرگر معمولی با قارچ و پنیر",
+    price: 12000,
+    id: "cheeseburger",
+    imgSrc: burger,
+  },
+  {
+    title: "همبرگر مخصوص با قارچ و پنیر",
+    price: 14000,
+    id: "cheeseSpecialburger",
+    imgSrc: burger,
+  },
+  {
+    title: "سیب زمینی سرخ کرده ویژه",
+    price: 14000,
+    id: "specialPotato",
+    imgSrc: sib,
+  },
+  {
+    title: "سیب زمینی سرخ کرده",
+    price: 10000,
+    id: "Potato",
+    imgSrc: sib,
+  },
+  {
+    title: "سالاد فصل",
+    price: 4000,
+    id: "salad",
+    imgSrc: salad,
+  },
+  {
+    title: "نوشابه",
+    price: 4000,
+    id: "soda",
+    imgSrc: soda,
+  },
+];
 
 function App() {
+  const [total, setTotal] = useState(0);
+  function billing(value) {
+    setTotal(total + value);
+  }
   return (
     <>
       <div className={"container"}>
         <div className={"leftBox"}>
           <div className="text">
             <label>جمع کل سفارشات:</label>
-            <span>0 تومان</span>
+            <span>{total} تومان</span>
           </div>
           <div className="text">
             <label>حق سرویس و کارمزد:</label>
@@ -21,9 +81,7 @@ function App() {
           </div>
           <div className="Discount">
             <input type="text" placeholder={"کد تخفیف"}></input>
-            <div>
-              
-            </div>
+            <div></div>
           </div>
           <div className="totalPrice text">
             <label>مبلغ قابل پرداخت:</label>
@@ -36,18 +94,17 @@ function App() {
         <div className={"rightBox"}>
           <footer className="footer-box">رستوران مک دونالد شعبه تهران</footer>
           <div id="foodContainer">
-            <Card
-              id="burger"
-              title={"همبرگر معمولی"}
-              price={"10000"}
-              imgSrc={bergerImage}
-            />
-            <Card
-              id="burger2"
-              title={"همبرگر معمولی"}
-              price={"10000"}
-              imgSrc={""}
-            />
+            {foods.map((food) => {
+              return (
+                <Card
+                  id={food.id}
+                  title={food.title}
+                  price={food.price}
+                  imgSrc={food.imgSrc}
+                  totalPrice={billing}
+                />
+              );
+            })}
           </div>
         </div>
       </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,23 @@
 import { useState } from "react"
 import "./Card.css"
 
-function Card({id, title, price, imgSrc}){
+function Card({id, title, price, imgSrc, totalPrice}){
     const [count, setCount] = useState(0)
     return(
         <div className="product" id={id}>
             <img src={imgSrc} alt="" />
             <div className="product__spec d-flex f-d-c j-s-e w-full">
-              <h3>{title}</h3>
+              <h4>{title}</h4>
               <h5>{price} تومان</h5>
               <div className="d-flex j-s-b">
                 <div className="product__spec__counter">
-                  <a className="plusBtn" title="افزودن" onClick={()=>{setCount(count+1)}}
+                  <a className="plusBtn" title="افزودن" onClick={()=>{setCount(count+1)
+                  totalPrice(price)}}
                     ><span className="bi bi-plus-square-fill plusBtn">+</span
                   ></a>
                   <label>{count}</label>
-                  <a className="minusBtn" title="حذف" onClick={()=>{if(count>0) setCount(count-1)}}
+                  <a className="minusBtn" title="حذف" onClick={()=>{if(count>0) setCount(count-1)
+                  totalPrice(-price)}}
                     ><span className="bi bi-dash-square-fill minusBtn">-</span
                   ></a>
                 </div>
