b68449250dbafea5528c6ffb5a8901c2264ac2c9
Morteza Javanbakht <114153047+MortezaJavanbakht@users.noreply.github.com>
2023-01-06 15:23:02 +0330

    Create Card

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
 import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
-  <>
-  <div className={'container'}>
-    <div className={'leftBox'}>
-      <div>
-        <label>جمع کل سفارشات:</label>
-        <span>0 تومان</span>
+    <>
+      <div className={"container"}>
+        <div className={"leftBox"}>
+          <div>
+            <label>جمع کل سفارشات:</label>
+            <span>0 تومان</span>
+          </div>
+          <div>
+            <label>حق سرویس و کارمزد:</label>
+            <span>0 تومان</span>
+          </div>
+          <div>
+            <label>تخفیف:</label>
+            <span>0 درصد</span>
+          </div>
+          <div>
+            <input type="text" placeholder={"کد تخفیف"}></input>
+            <div>
+              <i class="bi bi-check-square-fill"></i>
+            </div>
+          </div>
+          <div className="totalPrice">
+            <label>مبلغ قابل پرداخت:</label>
+            <span>0 تومان</span>
+          </div>
+          <div typeof="submit" className="subBtn">
+            ثبت سفارش
+          </div>
+        </div>
+        <div className={"rightBox"}>
+          <footer className="footer-box">رستوران مک دونالد شعبه تهران</footer>
+          <div id="foodContainer">
+            <Card
+              id="burger"
+              title={"همبرگر معمولی"}
+              price={"10000"}
+              imgSrc={""}
+            />
+            <Card
+              id="burger2"
+              title={"همبرگر معمولی"}
+              price={"10000"}
+              imgSrc={""}
+            />
+          </div>
+        </div>
       </div>
-      <div>
-      <label>حق سرویس و کارمزد:</label>
-      <span>0 تومان</span>
-      </div>
-      <div>
-      <label>تخفیف:</label>
-      <span>0 درصد</span>
-      </div>
-      <div>
-      <input type='text' placeholder={"کد تخفیف"}></input>
-      <div><i class="bi bi-check-square-fill"></i></div>
-      </div>
-      <div className="totalPrice">
-      <label>مبلغ قابل پرداخت:</label>
-      <span>0 تومان</span>
-      </div>
-      <div typeof="submit" className="subBtn">ثبت سفارش</div>
-    </div>
-    <div className={'rightBox'}>
-      <footer className="footer-box">رستوران مک دونالد شعبه تهران</footer>
-    </div>
-  </div>
-  </>
-  )
+    </>
+  );
 }
 
 export default App;
